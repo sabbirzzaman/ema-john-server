b54725ad2d80e86f8e4fdb19bfe3b2066d8b0219
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-24 23:14:22 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -7,11 +7,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 
-app.get('/', (req,res) => {
-    res.send("Ema-john server is running")
-})
+// connect mongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b7sdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+    try{
+        console.log("MongoDB connected")
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+//
+app.get('/', (req, res) => {
+    res.send('Ema-john server is running');
+});
 
 app.listen(port, () => {
-    console.log("server is running")
-})
+    console.log('server is running');
+});
