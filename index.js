2246ffd5dc66116f056fe7cc564c33a20bd5de6f
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-25 00:27:43 +0600

    create product api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,8 +10,7 @@ app.use(express.json());
 
 // connect mongoDB
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b7sdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b7sdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -18,21 +18,28 @@ const client = new MongoClient(uri, {
 });
 
 const run = async () => {
-    try{
-        console.log("MongoDB connected")
+    try {
+        await client.connect();
+        const productsCollection = client.db('emaJohn').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+
+            res.send(products)
+        })
+    } finally {
     }
-    finally{
+};
 
-    }
-}
+run().catch(console.dir);
 
-run().catch(console.dir)
-
-//
 app.get('/', (req, res) => {
     res.send('Ema-john server is running');
 });
 
+// listen to the port
 app.listen(port, () => {
     console.log('server is running');
 });
